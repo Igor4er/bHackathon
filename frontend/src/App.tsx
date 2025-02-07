0fd25438da502bfb63b70c7022fe0def813efd9f
SashaKuh <oleksakukh@gmail.com>
2025-02-07 16:05:59 +0200

    login front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { FC } from "react";
+import CustomHistoryRouter from "./routes/CustomHistoryRouter";
+import { history } from "./services/history";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function App() {
-  return (
-    <>
-      <Button>Shadcn button</Button>
-    </>
-  );
+
+import AppRoutes from "./routes/Router";
+
+const queryClient = new QueryClient();
+
+const App: FC = () => {
+    return(
+        <QueryClientProvider client={queryClient}>
+        <CustomHistoryRouter history={history}>
+            <div className="App flex h-screen flex-col overflow-y-auto overflow-x-hidden">
+             <AppRoutes />
+        </div>
+        </CustomHistoryRouter>
+        </QueryClientProvider>
+        
+    )
 }
 
 export default App;
---
+++ frontend/src/components/Home/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { PageWrapper } from "../Layouts/PageWrapper";
+
+export const Home: FC = () => {
+    return (
+        <PageWrapper className="flex flex-col gap-10">
+            <h1>Home</h1>
+        </PageWrapper>
+    )
+} 
